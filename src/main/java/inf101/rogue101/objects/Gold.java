5270ffed84693cad670830dc40efb5d3a047f34f
Stian Munkejord <smu027@uib.no>
2021-03-05 14:57:24 +0100

    JAA!

---
+++ src/main/java/inf101/rogue101/objects/Gold.java
@@ -0,0 +1,51 @@
+package inf101.rogue101.objects;
+
+public class Gold implements IItem{
+
+
+    public static final char SYMBOL = 'G';
+
+    private int hp = getMaxHealth();
+
+    @Override
+    public int getCurrentHealth() {
+        return hp;
+    }
+
+    @Override
+    public int getDefence() {
+        return 0;
+    }
+
+    @Override
+    public int getMaxHealth() {
+        return 15;
+    }
+
+    @Override
+    public String getLongName() {
+        return "Cash money Cash";
+    }
+
+    @Override
+    public String getShortName() {
+        return "GØLD";
+    }
+
+    @Override
+    public int getSize() {
+        return 4;
+    }
+
+    @Override
+    public int handleDamage(int amount) {
+        int damage = Math.min(amount, hp);
+        hp -= damage;
+        return damage;
+    }
+
+    @Override
+    public char getSymbol() {
+        return SYMBOL;
+    }
+}
