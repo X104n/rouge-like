c25856b2d08c4c65c514fda58fae90076b2bb61c
Stian Munkejord <smu027@uib.no>
2021-03-10 18:43:39 +0100

    Walla balla

--- src/main/java/inf101/rogue101/map/GameMap.java
+++ src/main/java/inf101/rogue101/map/GameMap.java
@@ -14,327 +14,321 @@ import inf101.rogue101.objects.IItemComparator;
 import inf101.rogue101.objects.Wall;
 
 public class GameMap implements IGameMap {
-	/**
-	 * The grid that makes up our map
-	 */
-	private final IMultiGrid<IItem> grid;
+    /**
+     * The grid that makes up our map
+     */
+    private final IMultiGrid<IItem> grid;
 
-	public GameMap(int rows, int columns) {
-		grid = new MultiGrid<>(rows, columns);
-	}
+    public GameMap(int rows, int columns) {
+        grid = new MultiGrid<>(rows, columns);
+    }
 
-	public GameMap(IGrid<IItem> inputGrid) {
-		this(inputGrid.numRows(),inputGrid.numColumns());
-		for(Location loc : inputGrid.locations()) {
-			IItem item = inputGrid.get(loc);
-			if(item != null)
-				add(loc, item);
-		}
-	}
+    public GameMap(IGrid<IItem> inputGrid) {
+        this(inputGrid.numRows(), inputGrid.numColumns());
+        for (Location loc : inputGrid.locations()) {
+            IItem item = inputGrid.get(loc);
+            if (item != null)
+                add(loc, item);
+        }
+    }
 
-	/**
-	 * This method adds an IItem to the given location.
-	 * Since the draw method only draws one element in each cell,
-	 * the first element is the one drawn
-	 * In this implementation of IGameMap IItem's at same location
-	 * should be sorted such that the IItem with largest size() is first.
-	 *
-	 */
-	@Override
-	public void add(Location loc, IItem item) {
+    /**
+     * This method adds an IItem to the given location.
+     * Since the draw method only draws one element in each cell,
+     * the first element is the one drawn
+     * In this implementation of IGameMap IItem's at same location
+     * should be sorted such that the IItem with largest size() is first.
+     */
+    @Override
+    public void add(Location loc, IItem item) {
 
-		// do the actual adding
-		List<IItem> list = grid.get(loc);
-		list.add(item);
-		Collections.sort(list, Collections.reverseOrder(new IItemComparator()));
-	}
+        // do the actual adding
+        List<IItem> list = grid.get(loc);
+        list.add(item);
+        Collections.sort(list, Collections.reverseOrder(new IItemComparator()));
+    }
 
-	/**
-	 * In this implementation of IGame an IActor can not go through Walls
-	 * and only one IActor can be at each cell
-	 */
-	@Override
-	public boolean isAvailable(Location to) {
-		return !grid.contains(to, (i) -> (i instanceof Wall || i instanceof IActor));
-	}
+    /**
+     * In this implementation of IGame an IActor can not go through Walls
+     * and only one IActor can be at each cell
+     */
+    @Override
+    public boolean isAvailable(Location to) {
+        return !grid.contains(to, (i) -> (i instanceof Wall || i instanceof IActor));
+    }
 
-	@Override
-	public boolean hasNeighbour(Location from, GridDirection dir) {
-		return grid.canGo(from,dir);
-	}
+    @Override
+    public boolean hasNeighbour(Location from, GridDirection dir) {
+        return grid.canGo(from, dir);
+    }
 
-	@Override
-	public boolean canGo(Location from, GridDirection dir) {
-		if (!grid.canGo(from,dir))
-			return false;
-		if(dir.equals(GridDirection.CENTER))
-			return true;
-		Location loc = from.getNeighbor(dir);
-		return isAvailable(loc);
-	}
+    @Override
+    public boolean canGo(Location from, GridDirection dir) {
+        if (!grid.canGo(from, dir))
+            return false;
+        if (dir.equals(GridDirection.CENTER))
+            return true;
+        Location loc = from.getNeighbor(dir);
+        return isAvailable(loc);
+    }
 
-	/**
-	 *  In this implementation only 1 IActor can occupy each cell,
-	 *  hence the size() of the list returned is at most 1.
-	 */
-	@Override
-	public List<IActor> getActors(Location loc) {
-		List<IActor> items = new ArrayList<>();
-		for (IItem item : grid.get(loc)) {
-			if (item instanceof IActor)
-				items.add((IActor) item);
-		}
+    /**
+     * In this implementation only 1 IActor can occupy each cell,
+     * hence the size() of the list returned is at most 1.
+     */
+    @Override
+    public List<IActor> getActors(Location loc) {
+        List<IActor> items = new ArrayList<>();
+        for (IItem item : grid.get(loc)) {
+            if (item instanceof IActor)
+                items.add((IActor) item);
+        }
 
-		return items;
-	}
+        return items;
+    }
 
-	@Override
-	public List<IItem> getAll(Location loc) {
-		return Collections.unmodifiableList(grid.get(loc));
-	}
+    @Override
+    public List<IItem> getAll(Location loc) {
+        return Collections.unmodifiableList(grid.get(loc));
+    }
 
-	@Override
-	public List<IItem> getAllModifiable(Location loc) {
-		return grid.get(loc);
-	}
+    @Override
+    public List<IItem> getAllModifiable(Location loc) {
+        return grid.get(loc);
+    }
 
-	@Override
-	public void clean(Location loc) {
-		// remove any items that have health < 0:
-		grid.get(loc).removeIf((item) -> {
-			if (item.isDestroyed()) {
-				return true;
-			} else {
-				return false;
-			}
-		});
+    @Override
+    public void clean(Location loc) {
+        // remove any items that have health < 0:
+        grid.get(loc).removeIf((item) -> {
+            if (item.isDestroyed()) {
+                return true;
+            } else {
+                return false;
+            }
+        });
 
-	}
+    }
 
-	@Override
-	public int getHeight() {
-		return grid.numRows();
-	}
+    @Override
+    public int getHeight() {
+        return grid.numRows();
+    }
 
-	@Override
-	public List<IItem> getItems(Location loc) {
-		List<IItem> items = new ArrayList<>(grid.get(loc));
-		items.removeIf((i) -> i instanceof IActor);
-		return items;
-	}
+    @Override
+    public List<IItem> getItems(Location loc) {
+        List<IItem> items = new ArrayList<>(grid.get(loc));
+        items.removeIf((i) -> i instanceof IActor);
+        return items;
+    }
 
-	@Override
-	public Location getLocation(IItem item) {
-		for(Location loc : locations()) {
-			for(IItem cur : getAll(loc)) {
-				if(cur==item) {
-					return loc;
-				}
-			}
-		}
-		return null;
-	}
+    @Override
+    public Location getLocation(IItem item) {
+        for (Location loc : locations()) {
+            for (IItem cur : getAll(loc)) {
+                if (cur == item) {
+                    return loc;
+                }
+            }
+        }
+        return null;
+    }
 
-	@Override
-	public int getWidth() {
-		return grid.numColumns();
-	}
+    @Override
+    public int getWidth() {
+        return grid.numColumns();
+    }
 
-	@Override
-	public boolean has(Location loc, IItem target) {
-		return grid.contains(loc, target);
-	}
+    @Override
+    public boolean has(Location loc, IItem target) {
+        return grid.contains(loc, target);
+    }
 
-	@Override
-	public boolean hasActors(Location loc) {
-		return grid.contains(loc, (i) -> i instanceof IActor);
-	}
+    @Override
+    public boolean hasActors(Location loc) {
+        return grid.contains(loc, (i) -> i instanceof IActor);
+    }
 
-	@Override
-	public boolean hasItems(Location loc) {
-		// true if grid cell contains an item which is not an IActor
-		return grid.contains(loc, (i) -> !(i instanceof IActor));
-	}
+    @Override
+    public boolean hasItems(Location loc) {
+        // true if grid cell contains an item which is not an IActor
+        return grid.contains(loc, (i) -> !(i instanceof IActor));
+    }
 
-	@Override
-	public boolean hasWall(Location loc) {
-		return grid.contains(loc, (i) -> i instanceof Wall);
-	}
+    @Override
+    public boolean hasWall(Location loc) {
+        return grid.contains(loc, (i) -> i instanceof Wall);
+    }
 
-	@Override
-	public void remove(Location loc, IItem item) {
-		grid.remove(loc, item);
-	}
+    @Override
+    public void remove(Location loc, IItem item) {
+        grid.remove(loc, item);
+    }
 
-	@Override
-	public List<Location> getNeighbourhood(Location loc, int dist) {
-		if (dist < 0 || loc == null)
-			throw new IllegalArgumentException();
-		else if (dist == 0)
-			return new ArrayList<>(); // empty!
-		else if (dist > 0){
-			List<Location> positions = new ArrayList<>();
-			positions.add(loc);
+    @Override
+    public List<Location> getNeighbourhood(Location loc, int dist) {
+        if (dist < 0 || loc == null)
+            throw new IllegalArgumentException();
+        else if (dist == 0)
+            return new ArrayList<>(); // empty!
+        else if (dist > 0) {
+            List<Location> positions = new ArrayList<>();
+            positions.add(loc);
 
-			for (int i = 0; i < dist; i++) {
+            for (int i = 0; i < dist; i++) {
 
-				List<Location> temporary = new ArrayList<>();
+                List<Location> temporary = new ArrayList<>();
+                for (Location position : positions) {
 
-				for (Location position : positions){
+                    for (GridDirection dir : GridDirection.EIGHT_DIRECTIONS) {
 
-					for (GridDirection dir : GridDirection.EIGHT_DIRECTIONS) {
+                        temporary.add(position.getNeighbor(dir));
 
-						temporary.add(position.getNeighbor(dir));
+                    }
 
-					}
+                }
+                for (Location temp : temporary) {
 
-				}
-				for (Location temp : temporary){
+                    if (positions.contains(temp)) {
+                        continue;
+                    } else if (Math.abs(temp.col) != temp.col) {
+                        continue;
+                    } else if (Math.abs(temp.row) != temp.row) {
+                        continue;
+                    } else if (hasWall(temp)) {
+                        continue;
+                    } else {
+                        positions.add(temp);
+                    }
+                }
 
-					if(positions.contains(temp)){
-						continue;
-					}
-					else if (Math.abs(temp.col) != temp.col){
-						continue;
-					}
-					else if (Math.abs(temp.row) != temp.row){
-						continue;
-					}
-					else if (hasWall(temp)) {
-						continue;
-					}
-					else{
-						positions.add(temp);
-					}
-				}
+            }
 
-			}
-
-			positions.remove(loc);
-			Collections.sort(positions, new LocationComparator(loc));
-			return positions;
-		}
+            positions.remove(loc);
+            Collections.sort(positions, new LocationComparator(loc));
+            return positions;
+        }
 
 
-		throw new UnsupportedOperationException();
-	}
+        throw new UnsupportedOperationException();
+    }
 
 
-	@Override
-	public List<GridDirection> getPossibleMoves(Location currentLocation) {
+    @Override
+    public List<GridDirection> getPossibleMoves(Location currentLocation) {
 
-		int length1 = GridDirection.EIGHT_DIRECTIONS.size();
+        int length1 = GridDirection.EIGHT_DIRECTIONS.size();
 
-		ArrayList<GridDirection> output = new ArrayList<GridDirection>();
+        ArrayList<GridDirection> output = new ArrayList<GridDirection>();
 
-		for (int i = 0; i < length1; i++){
-			if (canGo(currentLocation, GridDirection.EIGHT_DIRECTIONS.get(i))){
-				output.add(GridDirection.EIGHT_DIRECTIONS.get(i));
-			}
-		}
+        for (int i = 0; i < length1; i++) {
+            if (canGo(currentLocation, GridDirection.EIGHT_DIRECTIONS.get(i))) {
+                output.add(GridDirection.EIGHT_DIRECTIONS.get(i));
+            }
+        }
 
-		return output;
-	}
+        return output;
+    }
 
-	@Override
-	public List<Location> getReachable(Location loc, int dist) {
-		List<Location> reachable = new ArrayList<>();
+    @Override
+    public List<Location> getReachable(Location loc, int dist) {
+        List<Location> reachable = new ArrayList<>();
 
-		for (Location location : getNeighbourhood(loc, dist)){
-			if (!containsItem(location, IItem.class)){
-				reachable.add(location);
-			}
-		}
-		return reachable;
+        for (Location location : getNeighbourhood(loc, dist)) {
+            if (!containsItem(location, IItem.class)) {
+                reachable.add(location);
+            }
+        }
+        return reachable;
 /***
-		if (dist < 0 || loc == null)
-			throw new IllegalArgumentException();
-		else if (dist == 0)
-			return new ArrayList<>(); // empty!
-		else if (dist > 0){
+ if (dist < 0 || loc == null)
+ throw new IllegalArgumentException();
+ else if (dist == 0)
+ return new ArrayList<>(); // empty!
+ else if (dist > 0){
 
-			List<Location> positions = new ArrayList<>();
-			positions.add(loc);
+ List<Location> positions = new ArrayList<>();
+ positions.add(loc);
 
-			for (int i = 0; i < dist; i++) {
+ for (int i = 0; i < dist; i++) {
 
-				List<Location> temporary = new ArrayList<>();
+ List<Location> temporary = new ArrayList<>();
 
-				//System.out.println("Listen er " + positions);
+ //System.out.println("Listen er " + positions);
 
-				for (Location position : positions){
+ for (Location position : positions){
 
-					for (GridDirection dir : GridDirection.EIGHT_DIRECTIONS) {
+ for (GridDirection dir : GridDirection.EIGHT_DIRECTIONS) {
 
-						if (!containsItem(position, IItem.class)){
+ if (!containsItem(position, IItem.class)){
 
-							temporary.add(position.getNeighbor(dir));
-						}
+ temporary.add(position.getNeighbor(dir));
+ }
 
-					}
+ }
 
-				}
-				for (Location temp : temporary){
+ }
+ for (Location temp : temporary){
 
-					if(positions.contains(temp)){
-						continue;
-					}
-					else if (Math.abs(temp.col) != temp.col){
-						continue;
-					}
-					else if (Math.abs(temp.row) != temp.row){
-						continue;
-					}
-					else if (hasWall(temp)) {
-						continue;
-					}
-					else{
-						positions.add(temp);
-					}
-				}
+ if(positions.contains(temp)){
+ continue;
+ }
+ else if (Math.abs(temp.col) != temp.col){
+ continue;
+ }
+ else if (Math.abs(temp.row) != temp.row){
+ continue;
+ }
+ else if (hasWall(temp)) {
+ continue;
+ }
+ else{
+ positions.add(temp);
+ }
+ }
 
-			}
+ }
 
-			positions.remove(loc);
-
-
-			Collections.sort(positions, new LocationComparator(loc));
-
-			System.out.println(loc);
-
-			return positions;
-
-		}
+ positions.remove(loc);
 
 
-		throw new UnsupportedOperationException();
+ Collections.sort(positions, new LocationComparator(loc));
+
+ System.out.println(loc);
+
+ return positions;
+
+ }
+
+
+ throw new UnsupportedOperationException();
  */
-	}
+    }
 
-	@Override
-	public Iterable<Location> locations() {
-		return grid.locations();
-	}
+    @Override
+    public Iterable<Location> locations() {
+        return grid.locations();
+    }
 
-	@Override
-	public <T extends IItem> boolean containsItem(Location loc, Class<T> c) {
-		List<IItem> items = getAll(loc);
-		return containsItem(items, c);
-	}
+    @Override
+    public <T extends IItem> boolean containsItem(Location loc, Class<T> c) {
+        List<IItem> items = getAll(loc);
+        return containsItem(items, c);
+    }
 
-	/**
-	 * This is a helper method that searches through a list to see if there are any
-	 * instances of a particular type.
-	 *
-	 * @param items the list to search through
-	 * @param c the type to search for
-	 * @return true if an instance was found, false otherwise
-	 */
-	public static boolean containsItem(List<?> items, Class<?> c) {
-		for (Object item : items) {
-			if (c.isInstance(item))
-				return true;
-		}
-		return false;
-	}
+    /**
+     * This is a helper method that searches through a list to see if there are any
+     * instances of a particular type.
+     *
+     * @param items the list to search through
+     * @param c     the type to search for
+     * @return true if an instance was found, false otherwise
+     */
+    public static boolean containsItem(List<?> items, Class<?> c) {
+        for (Object item : items) {
+            if (c.isInstance(item))
+                return true;
+        }
+        return false;
+    }
 }
--- src/main/java/inf101/rogue101/objects/Rabbit.java
+++ src/main/java/inf101/rogue101/objects/Rabbit.java
@@ -10,168 +10,187 @@ import inf101.rogue101.game.IGameView;
 import javafx.scene.paint.Color;
 
 /**
- * Kaniner i spillet Rogue 101. 
- * Kaniner hopper rundt i labyrinten på jakt etter gulrøtter. 
- * De har god luktesans og kan lukte gulrøtter på lang avstand. 
- * 
- * De forbrenner energi raskt og må være effektiv 
- * 
+ * Kaniner i spillet Rogue 101.
+ * Kaniner hopper rundt i labyrinten på jakt etter gulrøtter.
+ * De har god luktesans og kan lukte gulrøtter på lang avstand.
+ * <p>
+ * De forbrenner energi raskt og må være effektiv
+ *
  * @author Knut Anders Stokke
  * @author Martin Vatshelle
- *
  */
 public class Rabbit implements IActor {
-	/**
-	 * char representation of this type 
-	 */
-	public static final char SYMBOL = 'R';
-	private int hp = getMaxHealth();
+    /**
+     * char representation of this type
+     */
+    public static final char SYMBOL = 'R';
+    private int hp = getMaxHealth();
 
-	@Override
-	public void doTurn(IGameView game) {
-		boolean isHungry = getMaxHealth()-getCurrentHealth()>2;
-		if(isHungry) {
-			boolean gotFood = eatIfPossible(game);
-			if (gotFood) {
-				//Rabbit is full and prefers to rest
-				return;
-			}
-		}
-		GridDirection dir = selectMove(game);
-		performMove(game,dir);
-	}
+    @Override
+    public void doTurn(IGameView game) {
+        boolean isHungry = getMaxHealth() - getCurrentHealth() > 2;
+        if (isHungry) {
+            boolean gotFood = eatIfPossible(game);
+            if (gotFood) {
+                //Rabbit is full and prefers to rest
+                return;
+            }
+        }
 
-	/**
-	 * This method selects which move the Rabbit want to make.
-	 */
-	private GridDirection selectMove(IGameView game) {
-		List<GridDirection> possibleMoves = game.getPossibleMoves();
-		List<GridDirection> CarrotMoves = new ArrayList<>();
-		boolean carrotBool = false;
 
-		for (GridDirection direction : GridDirection.EIGHT_DIRECTIONS){
-			if (game.containsItem(direction, Carrot.class)){
-				CarrotMoves.add(direction);
-				carrotBool = true;
-			}
-		}
+        GridDirection dir = selectMove(game);
 
-		if (carrotBool){
-			Collections.shuffle(CarrotMoves);
-			return CarrotMoves.get(0);
-		}
-		Collections.shuffle(possibleMoves);
-		return possibleMoves.get(0);
-	}
+        performMove(game, dir);
+    }
 
-	/**
-	 * Burns one round of energy
-	 * Rabbits only burn energy if they move, not when laying still.
-	 */
-	private void burnEnergy() {
-		hp--;
-	}
+    /**
+     * This method selects which move the Rabbit want to make.
+     */
+    private GridDirection selectMove(IGameView game) {
+        List<GridDirection> possibleMoves = game.getPossibleMoves();
+        List<GridDirection> CarrotMoves = new ArrayList<>();
+        List<IItem> NearbyItems = game.getNearbyItems(5);
+        boolean carrotBool = false;
 
-	/**
-	 * Performs a move
-	 * Every time a Rabbit moves it burns energy
-	 * GridDirection.CENTER means the Rabbit is resting and not burning energy
-	 * 
-	 * @param game
-	 */
-	private void performMove(IGameView game, GridDirection dir) {
-		if (!dir.equals(GridDirection.CENTER)) {
-			burnEnergy();
-			boolean moved = game.move(dir);
-			if(!moved) {
-				game.displayMessage("Rabbit wasted energy on move");
-			}
-		}
-	}
+        for (GridDirection direction : GridDirection.EIGHT_DIRECTIONS) {
+            if (game.containsItem(direction, Carrot.class)) {
+                CarrotMoves.add(direction);
+                carrotBool = true;
+            }
+        }
 
-	/**
-	 * Eats carrot if any exist at current location
-	 * 
-	 * @param game
-	 *            The game the object exists in
-	 * @return true if it spend the turn eating
-	 */
-	private boolean eatIfPossible(IGameView game) {
-		for (IItem item : game.getLocalNonActorItems()) {
-			if (item instanceof Carrot) {
-				System.out.println("found carrot!");
-				int eaten = item.handleDamage(Math.min(5, getMaxHealth()-getCurrentHealth()));
-				if (eaten > 0) {
-					System.out.println("ate carrot worth " + eaten + "!");
-					hp += eaten;
-					game.displayMessage("You hear a faint crunching (" + getLongName() + " eats " + item.getArticle()
-							+ " " + item.getLongName() + ")");
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+        if (!carrotBool) {
+            for (IItem NearbyCarrots : NearbyItems) {
 
-	@Override
-	public int getAttack() {
-		return 5;
-	}
+                if (NearbyCarrots instanceof Carrot) {
 
-	@Override
-	public int getCurrentHealth() {
-		return hp;
-	}
+                    GridDirection temp = game.getDirectionTo(NearbyCarrots);
 
-	@Override
-	public int getDamage() {
-		return 8;
-	}
+                    return temp;
 
-	@Override
-	public int getDefence() {
-		return 2;
-	}
+                }
 
-	@Override
-	public int getMaxHealth() {
-		return 10;
-	}
+            }
 
-	@Override
-	public String getShortName() {
-		return getLongName();
-	}
 
-	public String getLongName() {
-		return "rabbit";
-	}
+        }
 
-	@Override
-	public int getSize() {
-		return 4;
-	}
 
-	@Override
-	public String getGraphicTextSymbol() {
-		return hp > 0 ? "" + SYMBOL : "¤";
-	}
-	
-	@Override
-	public String getEmoji() {
-		return hp > 0 ? Printer.coloured("🐰", Color.LIGHTPINK) : "💀"; // 🐇
-	}
+        if (carrotBool) {
+            Collections.shuffle(CarrotMoves);
+            return CarrotMoves.get(0);
+        }
+        Collections.shuffle(possibleMoves);
+        return possibleMoves.get(0);
+    }
 
-	@Override
-	public int handleDamage(int amount) {
-		amount -= Math.min(amount, getDefence());
-		int damage = Math.min(amount, hp);
-		hp -= damage;
-		return damage;
-	}
-	
-	@Override
-	public char getSymbol() {
-		return SYMBOL;
-	}
+    /**
+     * Burns one round of energy
+     * Rabbits only burn energy if they move, not when laying still.
+     */
+    private void burnEnergy() {
+        hp--;
+    }
+
+    /**
+     * Performs a move
+     * Every time a Rabbit moves it burns energy
+     * GridDirection.CENTER means the Rabbit is resting and not burning energy
+     *
+     * @param game
+     */
+    private void performMove(IGameView game, GridDirection dir) {
+        if (!dir.equals(GridDirection.CENTER)) {
+            burnEnergy();
+            boolean moved = game.move(dir);
+            if (!moved) {
+                game.displayMessage("Rabbit wasted energy on move");
+            }
+        }
+    }
+
+    /**
+     * Eats carrot if any exist at current location
+     *
+     * @param game The game the object exists in
+     * @return true if it spend the turn eating
+     */
+    private boolean eatIfPossible(IGameView game) {
+        for (IItem item : game.getLocalNonActorItems()) {
+            if (item instanceof Carrot) {
+                System.out.println("found carrot!");
+                int eaten = item.handleDamage(Math.min(5, getMaxHealth() - getCurrentHealth()));
+                if (eaten > 0) {
+                    System.out.println("ate carrot worth " + eaten + "!");
+                    hp += eaten;
+                    game.displayMessage("You hear a faint crunching (" + getLongName() + " eats " + item.getArticle()
+                            + " " + item.getLongName() + ")");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int getAttack() {
+        return 5;
+    }
+
+    @Override
+    public int getCurrentHealth() {
+        return hp;
+    }
+
+    @Override
+    public int getDamage() {
+        return 8;
+    }
+
+    @Override
+    public int getDefence() {
+        return 2;
+    }
+
+    @Override
+    public int getMaxHealth() {
+        return 10;
+    }
+
+    @Override
+    public String getShortName() {
+        return getLongName();
+    }
+
+    public String getLongName() {
+        return "rabbit";
+    }
+
+    @Override
+    public int getSize() {
+        return 4;
+    }
+
+    @Override
+    public String getGraphicTextSymbol() {
+        return hp > 0 ? "" + SYMBOL : "¤";
+    }
+
+    @Override
+    public String getEmoji() {
+        return hp > 0 ? Printer.coloured("🐰", Color.LIGHTPINK) : "💀"; // 🐇
+    }
+
+    @Override
+    public int handleDamage(int amount) {
+        amount -= Math.min(amount, getDefence());
+        int damage = Math.min(amount, hp);
+        hp -= damage;
+        return damage;
+    }
+
+    @Override
+    public char getSymbol() {
+        return SYMBOL;
+    }
 }
